38268c6993c81d1b715f02ca116f9d00c9b818fa
Tim <41332362+CreedDE@users.noreply.github.com>
2021-07-27 20:08:22 +0200

    desktop sidebar + logic to detect if mobile or not

--- src/app/modules/layout/components/sidenav/sidenav.component.ts
+++ src/app/modules/layout/components/sidenav/sidenav.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'lema-sidenav',
 	templateUrl: './sidenav.component.html',
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit {
 	mobileMenu = false;
+	isMobile = false;
+
+	screenWidth: number;
+
+	ngOnInit(): void {
+		this.screenWidth = window.innerWidth;
+		if (window.innerWidth <= 1024) {
+			this.isMobile = true;
+		}
+	}
 
 	toggleMobileMenu(): void {
 		this.mobileMenu = !this.mobileMenu;
+	}
+
+	@HostListener('window:resize', ['$event'])
+	onResize(): void {
+		this.screenWidth = window.innerWidth;
+		if (window.innerWidth <= 1024) {
+			this.isMobile = true;
+		} else {
+			this.isMobile = false;
+		}
 	}
 }
