794c6ff769ab6a284bc7a92dec5ae563b50035ab
Tim <41332362+CreedDE@users.noreply.github.com>
2021-09-10 00:34:05 +0200

    added input as a reuseable component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LemaButtonModule } from './components/button/button.module';
+import { LemaInputModule } from './components/input/input.module';
 import { MaterialModule } from './modules/material.module';
 
 @NgModule({
-	imports: [CommonModule, MaterialModule, ReactiveFormsModule, FormsModule, LemaButtonModule],
+	imports: [CommonModule, MaterialModule, ReactiveFormsModule, FormsModule, LemaButtonModule, LemaInputModule],
 	declarations: [],
-	exports: [CommonModule, MaterialModule, ReactiveFormsModule, FormsModule, LemaButtonModule],
+	exports: [CommonModule, MaterialModule, ReactiveFormsModule, FormsModule, LemaButtonModule, LemaInputModule],
 })
 export class SharedModule {}
